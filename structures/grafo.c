97ab51ad3bd8d33c874624fb72e2794b45a6f844
Luis Eduardo <vsluiseduardo01@gmail.com>
2023-05-26 23:33:18 -0400

    feat(Estrutura): Grafo
    Modularizado o tipo grafo.

---
+++ structures/grafo.c
@@ -0,0 +1,47 @@
+#include "grafo.h"
+#include <stdlib.h>
+
+/**
+ * @brief Insere aresta no grafo e incrementa o número de arestas
+ * @param grafo Ponteiro para o grafo
+ * @param v vertice 1
+ * @param w vertice 2
+ * @param peso
+ */
+int inserirAresta(Grafo* grafo, int v, int w, int peso) {
+    grafo->A++; // Incrementar o número de arestas
+    Aresta* novaAresta = (Aresta*) realloc(grafo->arestas, grafo->A * sizeof(Aresta)); // Realocar memória para a nova aresta
+    if (novaAresta == NULL) {
+        // Tratar a falha de alocação de memória
+        return -1;
+    }
+    grafo->arestas = novaAresta; // Atribuir a nova memória realocada
+    int posicao = grafo->A - 1; // Posição correta no array de arestas
+    grafo->arestas[posicao].v = v;
+    grafo->arestas[posicao].w = w;
+    grafo->arestas[posicao].peso = peso;
+    return 0;
+}
+
+/**
+ * @brief Libera a memória alocada para o grafo e para o conjunto de arestas
+ * @param grafo Ponteiro para o grafo
+ */
+void deleteGrafo(Grafo* grafo) {
+    free(grafo->arestas);
+    free(grafo);
+}
+
+/**
+ * @brief Inicializa o grafo. Aloca memória para o grafo e para o conjunto de arestas e inicializa o número de vértices e arestas
+ * @param V Número de vértices
+ * @param A Número de arestas
+ * @return Ponteiro para o grafo
+ */
+Grafo* inicializaGrafo(int V, int A) {
+    Grafo* grafo = (Grafo*) malloc(sizeof(Grafo));
+    grafo->V = V;
+    grafo->A = 0;  // Iniciar o número de arestas como 0
+    grafo->arestas = NULL; // Inicialmente, não há arestas alocadas
+    return grafo;
+}
---
+++ structures/grafo.h
@@ -0,0 +1,42 @@
+//
+// Created by luis on 26/05/23.
+//
+
+#ifndef PPD_2023_SEQ_GRAFO_H
+#define PPD_2023_SEQ_GRAFO_H
+typedef struct {
+    int v; /*Vértice 1*/
+    int w; /*Vértice 2*/
+    int peso;
+} Aresta;
+
+typedef struct {
+    int V; /*Número de vertices no grafo*/
+    int A; /*Número de arestas no grafo*/
+    Aresta *arestas; /*Conjunto de arestas*/
+} Grafo;
+
+/**
+ * @brief Inicializa o grafo. Aloca memória para o grafo e para o conjunto de arestas e inicializa o número de vértices e arestas
+ * @param V Número de vértices
+ * @param A Número de arestas
+ * @return Ponteiro para o grafo
+ */
+Grafo* inicializaGrafo(int V, int A);
+/**
+ * @brief Insere aresta no grafo e incrementa o número de arestas
+ * @param grafo Ponteiro para o grafo
+ * @param v vertice 1
+ * @param w vertice 2
+ * @param peso
+ */
+int inserirAresta(Grafo* grafo, int v, int w, int peso);
+
+/**
+ * @brief Libera a memória alocada para o grafo e para o conjunto de arestas
+ * @param grafo Ponteiro para o grafo
+ */
+void deleteGrafo(Grafo* grafo);
+
+
+#endif //PPD_2023_SEQ_GRAFO_H
